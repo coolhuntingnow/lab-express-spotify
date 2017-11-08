90541911cea118d538dfe0ffeec053636e46420f
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-11-08 21:07:48 +0100

    Iteracion 3 paso 1 terminado

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,3 +1,4 @@
+// Pasquete API de Spotify
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
@@ -15,4 +16,39 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+})
+
+const express = require('express');
+const app = express();
+const expressLayouts     = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//You'll need a basic index route, that renders a home page.
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/artists', function (req, res) {
+  console.log(req);
+  let artistSearch = req.query.artist;
+  spotifyApi.searchArtists(artistSearch)
+  .then(function(data) {
+    let artistsArray = data.body.artists.items;
+    res.render('artists', {artists: artistsArray});
+
+  }, function(err) {
+    console.error(err);
+});
+});
+
+
+// El servidor esta escuchando en ese puerto
+app.listen(3000, () => {
+  console.log("Puerto 3000 Escuchando")
 });
